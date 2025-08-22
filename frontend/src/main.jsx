e8e4f92e53dc9786026bcbf314b8e7d1eecd3638
Harshal Dave <harshaldave5121@gmail.com>
2025-08-22 19:33:25 +0530

    Configure frontend for Netlify deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
-// Configure Axios to handle Django's CSRF protection
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-axios.defaults.withCredentials = true;
+// Set the base URL for Axios
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+} else {
+  axios.defaults.baseURL = 'http://127.0.0.1:8000';
+}
+
+axios.defaults.withCredentials = true; // Ensure cookies are sent with requests
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,5 +23,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
