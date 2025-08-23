c0c5b3bd1a4ff0bd6035f03dc589118f047b2300
Harshal Dave <harshaldave5121@gmail.com>
2025-08-23 22:28:08 +0530

    Add logging configuration for Heroku

--- capstone/settings.py
+++ capstone/settings.py
@@ -161,3 +161,26 @@ CSRF_TRUSTED_ORIGINS = [
     "https://68a888d2732a6e0007f54469--notes-rd.netlify.app",
     "https://notes-rd.netlify.app",
 ]
+
+
+# Logging configuration for Heroku
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
